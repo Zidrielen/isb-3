f6eac6ae5c5abead2b7501e8e198046550cbd460
Zaitcev Ilya <dp.io1866@yandex.ru>
2023-04-28 19:41:30 +0400

    A console menu has been made and a default json file has been added

---
+++ utils/system_functions.py
@@ -0,0 +1,18 @@
+import logging
+import json
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+def load_settings(settings_file: str) -> dict:
+    settings = None
+
+    try:
+        with open(settings_file) as json_file:
+            settings = json.load(json_file)
+        logging.info(f"Settings file successfully loaded from {settings_file}")
+
+    except OSError as err:
+        logging.warning(f"Settings file wasn't loaded from {settings_file}\n{err}")
+
+    return settings

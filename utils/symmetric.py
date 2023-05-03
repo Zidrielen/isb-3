3f1e3bfdb5b56df7a922f45fc0a46a7ced7e9635
Zaitcev Ilya <dp.io1866@yandex.ru>
2023-05-03 10:30:19 +0400

    Problem solved. It was necessary to look at encryption specifically for ChaCha20, and not write off from theory according to the laboratory

--- utils/symmetric.py
+++ utils/symmetric.py
@@ -1,8 +1,7 @@
 import logging
 import os
 
-from cryptography.hazmat.primitives import padding
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -10,26 +9,26 @@ logging.getLogger().setLevel(logging.INFO)
 class symmetric:
     def __init__(self) -> None:
         '''Class for working with symmetric encryption algorithm'''
-        self.__symmetric_key = None
-        self.__extra_parameter = None
+        self.__key = None
+        self.__nonce = None
         
     def key_generated(self) -> None:
         '''The functiion generated a symmetric key and extra parameter'''
-        self.__symmetric_key = os.urandom(32)
-        self.__extra_parameter = os.urandom(16)
+        self.__key = os.urandom(32)
+        self.__nonce = os.urandom(16)
         logging.info(" Symmetric key and extra parameter of ChaCha20 algorithm successfully generated")
 
     def get_key(self) -> bytes:
         '''Getter for symmetric key'''
-        return self.__symmetric_key
+        return self.__key
     
     def set_key(self, new_key: bytes) -> None:
         '''Setter for symmetric key'''
-        self.__symmetric_key = new_key
+        self.__key = new_key
 
     sym_key = property(get_key, set_key)
 
-    def load_symmetric_key(self, symmetric_txt: str, extra_pam_txt: str) -> None:
+    def load_symmetric_key(self, key_txt: str, nonce_txt: str) -> None:
         '''
         The function loads a symmetric key and extra parameter from txt file
 
@@ -38,22 +37,22 @@ class symmetric:
         '''
 
         try:
-            with open(symmetric_txt, "rb") as key_file:
-                self.__symmetric_key = key_file.read()
-            logging.info(f" Symmetric key successfully loaded from {symmetric_txt}")
+            with open(key_txt, "rb") as key_file:
+                self.__key = key_file.read()
+            logging.info(f" Symmetric key successfully loaded from {key_txt}")
 
         except OSError as err:
-            logging.warning(f" Symmetric key was not loaded from file {symmetric_txt}\n{err}")
+            logging.warning(f" Symmetric key was not loaded from file {key_txt}\n{err}")
 
         try:
-            with open(extra_pam_txt, "rb") as extra_file:
-                self.__extra_parameter = extra_file.read()
-            logging.info(f" Extra parameter successfully loaded from {extra_pam_txt}")
+            with open(nonce_txt, "rb") as nonce_file:
+                self.__nonce = nonce_file.read()
+            logging.info(f" Extra parameter successfully loaded from {nonce_txt}")
 
         except OSError as err:
-            logging.warning(f" Symmetric key was not loaded from file {extra_pam_txt}\n{err}")
+            logging.warning(f" Symmetric key was not loaded from file {nonce_txt}\n{err}")
     
-    def save_symmetric_key(self, symmetric_txt: str, extra_pam_txt: str) -> None:
+    def save_symmetric_key(self, key_txt: str, nonce_txt: str) -> None:
         '''
         The functiion saves a symmetric key and extra parameter to .txt
 
@@ -62,38 +61,36 @@ class symmetric:
         '''
 
         try:
-            with open(symmetric_txt, "wb") as key_file:
-                key_file.write(self.__symmetric_key)
-            logging.info(f" Symmetric key successfully saved to {symmetric_txt}")
+            with open(key_txt, "wb") as key_file:
+                key_file.write(self.__key)
+            logging.info(f" Symmetric key successfully saved to {key_txt}")
         
         except OSError as err:
-            logging.warning(f" Symmetric key wasn't saved to {symmetric_txt}\n{err}")
+            logging.warning(f" Symmetric key wasn't saved to {key_txt}\n{err}")
 
         try:
-            with open(extra_pam_txt, "wb") as extra_file:
-                extra_file.write(self.__extra_parameter)
-            logging.info(f" Extra parameter successfully saved to {extra_pam_txt}")
+            with open(nonce_txt, "wb") as nonce_file:
+                nonce_file.write(self.__nonce)
+            logging.info(f" Extra parameter successfully saved to {nonce_txt}")
 
         except OSError as err:
-            logging.warning(f" Extra parameter wasn't saved to {extra_pam_txt}\n{err}")
+            logging.warning(f" Extra parameter wasn't saved to {nonce_txt}\n{err}")
 
     def symmetric_encrypt(self, txt: bytes) -> bytes:
         '''
         The function encrypts an input text using symmetric key
 
-        :param text - text for encryption
+        :param txt - text for encryption
         :return - encrypted text
         '''
         
-        padder = padding.ANSIX923(32).padder()
-        padder_text = padder.update(txt) + padder.finalize()
-        iv = os.urandom(32)
-        cipher = Cipher(algorithms.ChaCha20(self.__symmetric_key, self.__extra_parameter),
-                        modes.CBC(iv))
+        algorithm = algorithms.ChaCha20(self.__key, self.__nonce)
+        cipher = Cipher(algorithm, mode=None)
         encryptor = cipher.encryptor()
-        cipher_text = encryptor.update(padder_text) + encryptor.finalize()
-        logging.info(" Text encryption was successful")
-        return iv + cipher_text
+        cipher_text = encryptor.update(txt)
+        return cipher_text
+
+
     
     
         
--- utils/system_functions.py
+++ utils/system_functions.py
@@ -10,9 +10,9 @@ def load_settings(settings_file: str) -> dict:
     try:
         with open(settings_file) as json_file:
             settings = json.load(json_file)
-        logging.info(f"Settings file successfully loaded from {settings_file}")
+        logging.info(f" Settings file successfully loaded from {settings_file}")
 
     except OSError as err:
-        logging.warning(f"Settings file wasn't loaded from {settings_file}\n{err}")
+        logging.warning(f" Settings file wasn't loaded from {settings_file}\n{err}")
 
     return settings
